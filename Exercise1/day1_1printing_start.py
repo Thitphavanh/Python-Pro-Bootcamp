46f6b5f0b8c1ada68b481918abd768fbc34cd529
Thitphavanh <thitphavanh@gmail.com>
2022-11-16 07:59:58 +0700

    first commit

---
+++ Exercise1/day1_1printing_start.py
@@ -0,0 +1,6 @@
+# print("Day 1 - Python Print Function")
+# print("The function is is declared like this:")
+# print("print('what to print')")
+
+# print("Hello" + " " + "Hery")
+print("Hello " + input("What is your name?"))
---
+++ Exercise1/day1_4variables.py
@@ -0,0 +1,13 @@
+# name = input("What is your name? ")
+
+name = "Hery"
+print(name)
+
+
+name = "Angela"
+print(name)
+
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
