0108b288582b656b0b9682a9745e1d30f36674b6
Thitphavanh <thitphavanh@gmail.com>
2022-11-22 07:42:52 +0700

    update

---
+++ Exercise1/day1_5variables_naming.py
@@ -0,0 +1,9 @@
+name = "Hery"
+print(name)
+
+name = "Phongsavath"
+print(name)
+
+# user_name = input("What is your name?")
+# length = len(user_name)
+# print(length)
---
+++ Exercise1/day2_1primitive_data_types.py
@@ -0,0 +1,18 @@
+# Data Types
+
+# String
+"Phenomeno"[5]
+
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+
+123_456_789
+
+# Float
+3.14159
+
+# Boolean
+True
+False
