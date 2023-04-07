7ec8fb3021dd130a3e0a215473961c832393f466
Thitphavanh <thitphavanh@gmail.com>
2023-04-07 07:28:00 +0700

    updated

---
+++ Exercise1/day5_4for_loops_and_the_range.py
@@ -0,0 +1,13 @@
+# fruits = ["Apple", "Peach", "Orange"]
+
+# for f in fruits:
+#     print(f)
+
+# for number in range(1, 11, 3):
+#     print(number)
+
+
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
